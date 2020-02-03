f9d1fd2e8d0f2da43a382c51ee4c77ba5eabbd6d
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-02-03 10:18:39 -0500

    Styled cart

--- client/src/components/cart/NewItems/NewItems.jsx
+++ client/src/components/cart/NewItems/NewItems.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { Query } from "react-apollo";
 import { IS_LOGGED_IN, FIND_USER_CART } from '../../../graphql/queries';
 import { ApolloConsumer } from "react-apollo";
@@ -21,30 +22,42 @@ const NewItems = props => {
                         console.log(data);
                         const lastProduct = data.cart.products[data.cart.products.length-1];
                         return (
-                            <div className="new-items">
-                                <div className="cart-row">
-                                    <div className="first-box">
-                                        <div className="check-icon"></div>
-                                        <div className="sml-cart-img-container">
-                                            <img className="sml-cart-img" src={lastProduct.imageUrl} />
+                            <div>
+                                <div className="new-items">
+                                    <div className="cart-row">
+                                        <div className="first-box">
+                                            <div className="check-icon"></div>
+                                            <div className="sml-cart-img-container">
+                                                <img className="sml-cart-img" src={lastProduct.imageUrl} />
+                                            </div>
+                                            <p className="green-add-cart">Added to Cart</p>
                                         </div>
-                                        <p className="green-add-cart">Added to Cart</p>
-                                    </div>
-                                    <div className="second-box">
-                                        <p className="cart-header-new">
-                                            {`Cart Subtotal (${data.cart.products.length} items):`}
-                                        </p>
-                                        <p className="price-red-new">
-                                            {`$${cartSum(data.cart.products).toFixed(2)}`}
-                                        </p>
-                                        <button className="outline-btn">
-                                            Cart
-                                        </button>
-                                        <button className="proceed-btn">
-                                            {`Proceed to checkout (${data.cart.products.length} items)`}
-                                        </button>
+                                        <div className="second-box">
+                                            <p className="cart-header-new">
+                                                {`Cart Subtotal (${data.cart.products.length} items):`}
+                                            </p>
+                                            <p className="price-red-new">
+                                                {`$${cartSum(data.cart.products).toFixed(2)}`}
+                                            </p>
+                                            <button className="outline-btn" onClick={() => props.history.push('/cart')}>
+                                                Cart
+                                            </button>
+                                            <button className="proceed-btn">
+                                                {`Proceed to checkout (${data.cart.products.length} items)`}
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
+                                {/* <div>
+                                    <ul>
+                                        {console.log(data.cart.products[0])}
+                                        {data.cart.products.map((prod) => (
+                                            <li>
+                                                <h1>{prod.name}</h1>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div> */}
                             </div>
                         )
                     } else {
@@ -59,6 +72,6 @@ const NewItems = props => {
     </ApolloConsumer>
 }
 
-export default NewItems;
+export default withRouter(NewItems);
 
 
